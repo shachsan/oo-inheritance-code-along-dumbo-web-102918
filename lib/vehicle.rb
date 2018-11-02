1c465e33d9b6a6b68d6c1f63ef603b558ad6c11c
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 16:12:02 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,5 +1,5 @@
 class Vehicle
-  attr_reader :wheel_size, :
+  attr_reader :wheel_size, :wheel_number
   def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
     @wheel_number = wheel_number
